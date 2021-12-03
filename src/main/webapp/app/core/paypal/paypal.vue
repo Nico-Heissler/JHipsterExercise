6d85a9fb400743656c7725f31b73553300b479aa
Quentin D <troisty84@gmail.com>
2021-12-03 15:39:35 +0100

    Final api paypal modification vue

--- src/main/webapp/app/core/paypal/paypal.vue
+++ src/main/webapp/app/core/paypal/paypal.vue
@@ -1,8 +1,9 @@
 <template>
 <div style="text-align: center;">
   <div id="paypal-button-container"></div>
-  <a href="cart" class="btn btn-outline-warning btn-dark btn-pill btn-md" data-abc="true"> Return to Cart </a>
+  <a href="cart" class="btn btn-outline-light btn-dark btn-pill btn-md" data-abc="true"> Return to Cart </a>
 </div>
+
 </template>
 
 <script src="https://www.paypal.com/sdk/js?client-id=AUUWIuQ5nApEGaKVo6TsLot_kfeB7fQLLz0V0AFbOgaHv_YP6RstZTusCFr8l6l_R6Bcz-1G_xaANUs3&currency=USD"></script>
